30fca9f0afe8799048bf4cc3eb96f432842e1fec
Sahin <sahinyanlik@gmail.com>
2024-05-25 20:44:56 +0000

    Props will be send in object array. But need to learn how to iterate in dom

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,57 @@
 const Header = (props) => {
-  console.log(props.course);
   return <h1>{props.course}</h1>;
 };
 
 const Content = (props) => {
-  console.log(props);
   return (
       <div>
-        <Part part={props.part1} exercises={props.exercise1} />
-        <Part part={props.part2} exercises={props.exercise2} />
-        <Part part={props.part3} exercises={props.exercise3} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
       </div>
   );
 };
 
 const Part = (props) => {
-  console.log(props);
+  console.log(props)
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   );
 };
 
 const Total = (props) => {
-  console.log(props);
   return (
     <p>
-      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1}  exercise1={exercises1} part2={part2}  exercise2={exercises2} part3={part3}  exercise3={exercises3}/>
+      <Content parts={parts}/>
       <Total
-        exercise1={exercises1}
-        exercise2={exercises2}
-        exercise3={exercises3}
+       parts={parts}
       />
     </div>
   );
